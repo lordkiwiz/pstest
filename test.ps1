61cbe0eb59977f18ed5e1a82165a49daf729b9a3
lordkiwiz <menno@hetepost.com>
2024-11-26 11:47:15 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,17 @@
+$user = 'admin'
+$pass = 'admin'
+
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
 gc welcome.txt
 gc welcome.txt | measure -word
 netstat -ano
-iwr http://127.0.0.1:1225
+iwr http://127.0.0.1:1225 -headers $headers
