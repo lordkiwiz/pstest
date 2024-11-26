b69d5f2659461d1e9d64481bc96f083ecdf14e8c
lordkiwiz <menno@hetepost.com>
2024-11-26 14:50:19 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -12,19 +12,18 @@ $Headers = @{
 }
 
 gc welcome.txt
-sleep 1
+start-sleep 1
 gc welcome.txt | measure -word
-sleep 1
+start-sleep 1
 netstat -ano
-sleep 1
+start-sleep 1
 iwr http://127.0.0.1:1225
-sleep 1
-
+start-sleep 1
 (iwr http://127.0.0.1:1225 -Headers $Headers).links.href | %{(iwr $_)} | %{if(($_.content | measure -word ).words -eq 138){$_.content}}
-sleep 1
+start-sleep 1
 (iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content
-sleep 1
+start-sleep 1
 $sha = ((iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content.split("`n") | ?{$_ -notlike "#*" -and $_ -notlike "*REDACTED*" -and $_ -notlike "file_MD5hash*"}).split(",")[1]
-sleep 1
+start-sleep 1
 iwr -Headers $Headers http://127.0.0.1:1225/tokens/$sha
-sleep 1
+start-sleep 1
