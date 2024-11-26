b449340ab198bcd0a668af43391c0ad85e751bc4
lordkiwiz <menno@hetepost.com>
2024-11-26 15:11:25 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -21,11 +21,11 @@ iwr http://127.0.0.1:1225
 start-sleep 1
 iwr http://127.0.0.1:1225 -Headers $Headers
 start-sleep 1
-(iwr http://127.0.0.1:1225 -Headers $Headers).links.href | %{(iwr $_)} | %{if(($_.content | measure -word ).words -eq 138){$_.content}}
-start-sleep 1
-(iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content
-start-sleep 1
-$sha = ((iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content.split("`n") | ?{$_ -notlike "#*" -and $_ -notlike "*REDACTED*" -and $_ -notlike "file_MD5hash*"}).split(",")[1]
-start-sleep 1
-iwr -Headers $Headers http://127.0.0.1:1225/tokens/$sha
-start-sleep 1
+#(iwr http://127.0.0.1:1225 -Headers $Headers).links.href | %{(iwr $_)} | %{if(($_.content | measure -word ).words -eq 138){$_.content}}
+#start-sleep 1
+#(iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content
+#start-sleep 1
+#$sha = ((iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content.split("`n") | ?{$_ -notlike "#*" -and $_ -notlike "*REDACTED*" -and $_ -notlike "file_MD5hash*"}).split(",")[1]
+#start-sleep 1
+#iwr -Headers $Headers http://127.0.0.1:1225/tokens/$sha
+#start-sleep 1
