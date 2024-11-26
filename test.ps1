0b66244838670ddd0e994dd62640167874281800
lordkiwiz <menno@hetepost.com>
2024-11-26 12:38:34 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -12,4 +12,4 @@ $Headers = @{
 }
 
 (iwr http://127.0.0.1:1225 -Headers $Headers).links.href | %{(iwr $_)} | %{if(($_.content | measure -word ).words -eq 138){$_.content}}
-iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers
+(iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content
