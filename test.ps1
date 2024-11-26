4a43deecf5692c4ca05c45cc4398e358a8524ef8
lordkiwiz <menno@hetepost.com>
2024-11-26 12:57:15 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -13,3 +13,6 @@ $Headers = @{
 
 (iwr http://127.0.0.1:1225 -Headers $Headers).links.href | %{(iwr $_)} | %{if(($_.content | measure -word ).words -eq 138){$_.content}}
 (iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content
+
+$sha = ((iwr http://127.0.0.1:1225/token_overview.csv -Headers $Headers).content.split("`n") | ?{$_ -notlike "#*" -and $_ -notlike "*REDACTED*" -and $_ -notlike "file_MD5hash*"}).split(",")[1]
+http://127.0.0.1:1225/tokens/$sha
